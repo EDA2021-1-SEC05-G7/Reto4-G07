6298de3d993a671e8bd22659c71ab415691d0938
aspd-2501 <a.padillad@uniandes.edu.co>
2021-06-06 13:15:55 -0500

    view casi listo xd

--- App/view.py
+++ App/view.py
@@ -26,6 +26,9 @@ import controller
 from DISClib.ADT import list as lt
 assert cf
 from DISClib.ADT import map as mp
+from DISClib.ADT import graph as gr
+from DISClib.ADT import stack as st
+from DISClib.ADT import minpq as pq
 
 
 """
@@ -38,11 +41,11 @@ def printMenu():
     print("*******************************************")
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
-    print("3- ")
-    print("4- ")
+    print("2- Encontrar la cantidad de clusters")
+    print("3- Landing points de interconexion")
+    print("4- Ruta minima entre dos paises")
     print("5- ")
-    print("6- ")
+    print("6- Impacto de fallo en landing point")
     print("7- ")
     print("0- Salir")
     print("*******************************************")
@@ -56,44 +59,84 @@ Menu principal
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
-    
+
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
-        catalog = controller.ini()
-        info = controller.loadData(catalog)
-        numvertex = controller.totalVertex(catalog)
-        numedges = controller.totalEdges(catalog)
-        countries = mp.size(catalog["Countries"])
-        print('Numero de Landing Pointa: ' + str(numvertex))
-        print('Numero de conexiones entre Landing points: ' + str(numedges))
-        print("Total de paises:", countries)
-        print(info)
 
+        catalog, flandingpoint, last_country = controller.initialize()
+        
+        print("Numero de landing points:", mp.size(catalog["LandingPoints"]))
+        print("Numero de conexiones entre landing points:", gr.numEdges(catalog["Connections"]))
+        print("Numero de paises:", mp.size(catalog["Countries"]))
+        print("Primer landing point:", "Id:", flandingpoint["id"], "Nombre:", flandingpoint["name"])
+        print("\tLatitude:", flandingpoint["latitude"].strip())
+        print("\tLongitude:", flandingpoint["longitude"].strip())
+        print("Ultimo pais:")
+        print("\tNombre:", last_country["CountryName"].strip())
+        print("\tPoblacion:", last_country["Population"].strip())
+        print("\tUsuarios de internet:", last_country["Internet users"].strip())
 
-    
     elif int(inputs[0]) == 2:
         #Requerimiento1
-        pass
+        landingpoint1 = input("Landing point 1:\n").strip()
+        landingpoint2 = input("Landing point 2:\n").strip()
 
-    
+        clusters, connected = controller.req1(catalog, landingpoint1, landingpoint2)
+
+        print("Numero de clusters:", clusters)
+        print("Mismo cluster:", ("yes" if connected else "no"))
+
+
     elif int(inputs[0]) == 3:
         #Requerimiento2
-        pass
-    
-    
+        ans = controller.req2(catalog)
+
+        for key in ans:
+            print("Landing point:", ans[key][0])
+            print("\tPais:", ans[key][1])
+            print("\tId:", ans[key][2])
+            print("\tCables connectados:", ans[key][3])
+
+
     elif int(inputs[0]) == 4:
         #Requerimiento3
-        pass
+        paisA = input("Pais A:\n").strip()
+        paisB = input("Pais B:\n").strip()
+
+        res = controller.req3(catalog, paisA, paisB)
+
+        if res == -1:
+            print("Informacion de pais no se pudo validar")
+            continue
+        if res == False:
+            print("No hay conexion entre estos dos paises")
+            continue
+
+        dist, route = res
+
+        while not st.isEmpty(route):
+            val = st.pop(route)
+            print("\tId1:", val["vertexA"], "Id2:", val["vertexB"], "Distancia:", val["weight"], "km")
+
+        print("Distancia Total:", dist)
 
 
     elif int(inputs[0]) == 5:
         #Requerimiento4
+        #Este requerimiento no se puede completar debido a que la puta implementacion de prim esta mal y no sirve ninguna de las funciones
         pass
 
-    
+
     elif int(inputs[0]) == 6:
         #Requerimiento5
-        pass
+        landingpoint = input("Landing point:\n").strip()
+        ans = controller.req5(catalog, landingpoint)
+
+        print("Paises afectados:", pq.size(ans))
+
+        while not pq.isEmpty(ans):
+            val = pq.delMin(ans)
+            print("\tPais:", val[0], "- Distancia:", val[1])
 
     elif int(inputs[0]) == 7:
         #Requerimiento6Bono
